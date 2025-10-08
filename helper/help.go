bdf71272e7f34747b880ee997df18bcefd960f18
Шарифзода Икбол <iqbolsharifzoda724@gmail.com>
2025-10-09 00:14:03 +0300

    Aded new file structures

--- helper/help.go
+++ helper/help.go
@@ -3,38 +3,12 @@ package helper
 import (
 	"database/sql"
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
-	"reflect"
 	"strconv"
-	"strings"
 )
 
-type Company struct {
-	ID        int    `json:"id"`
-	Name      string `json:"name"`
-	CreatedAt string `json:"-"`
-	UpdatedAt string `json:"-"`
-}
-type Posting struct {
-	ID          int    `json:"id"`
-	CompanyID   int    `json:"company_id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	CreatedAt   string `json:"-"`
-	UpdatedAt   string `json:"-"`
-}
-type Candidate struct {
-	ID        int    `json:"id"`
-	Name      string `json:"name"`
-	TelNumber string `json:"tel_number"`
-	Email     string `json:"email"`
-	CreatedAt string `json:"-"`
-	UpdatedAt string `json:"-"`
-}
-
-func Create[T any](db *sql.DB, w http.ResponseWriter, r *http.Request, tableName string) {
+func Create[T any](db *sql.DB, w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		http.Error(w, "Not allowed Method", http.StatusMethodNotAllowed)
 		return
@@ -46,96 +20,103 @@ func Create[T any](db *sql.DB, w http.ResponseWriter, r *http.Request, tableName
 		return
 	}
 	defer r.Body.Close()
-	var parametrs []string
-	var placeholders []string
-	var values []interface{}
-	counter := 0
-	t := reflect.TypeOf(item)
-	v := reflect.ValueOf(item)
-	for i := 0; i < t.NumField(); i++ {
-		field := t.Field(i)
-		fieldValue := v.Field(i)
-
-		jsonTag := field.Tag.Get("json")
-		if jsonTag == "-" || jsonTag == "id" {
-			continue
-		}
-		parametrs = append(parametrs, jsonTag)
-		counter++
-		placeholders = append(placeholders, fmt.Sprintf("$%d", counter))
-		values = append(values, fieldValue.Interface())
+	var result sql.Result
+	var err error = nil
+	switch any(item).(type) {
+	case Company:
+		query := `INSERT INTO companies (name, created_at, updated_at) VALUES ($1, NOW(), NOW())`
+		company := any(item).(Company)
+		result, err = db.Exec(query, company.Name)
+	case Candidate:
+		query := `INSERT INTO candidate (name,tel_number, email, created_at, updated_at) VALUES ($1, $2, $3, NOW(), NOW())`
+		candidate := any(item).(Candidate)
+		result, err = db.Exec(query, candidate.Name, candidate.TelNumber, candidate.Email)
+	case Posting:
+		query := `INSERT INTO job_postings (company_id, title, description, created_at, updated_at) VALUES ($1, $2, $3, NOW(), NOW())`
+		post := any(item).(Posting)
+		result, err = db.Exec(query, post.CompanyID, post.Title, post.Description)
 	}
-	parametrs = append(parametrs, "created_at", "updated_at")
-	placeholders = append(placeholders, fmt.Sprintf("$%d", counter+1), fmt.Sprintf("$%d", counter+2))
-	values = append(values, "NOW()", "NOW()")
-	query := fmt.Sprintf(
-		"INSERT INTO %s (%s) VALUES (%s)",
-		tableName,
-		strings.Join(parametrs, ", "),
-		strings.Join(placeholders, ", "),
-	)
-	result, err := db.Exec(query, values...)
 	if err != nil {
 		http.Error(w, "Server Error", http.StatusInternalServerError)
 		log.Println(err)
 		return
 	}
-	rowAffected, _ := result.RowsAffected()
-	if rowAffected == 0 {
-		http.Error(w, "Not Found", http.StatusNotFound)
+	affected, _ := result.RowsAffected()
+	if affected == 0 {
+		http.Error(w, "Not found", http.StatusNotFound)
 		return
 	}
 	w.WriteHeader(http.StatusOK)
 }
-func Read[T any](db *sql.DB, w http.ResponseWriter, r *http.Request, tableName string) {
+func Read[T any](db *sql.DB, w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		http.Error(w, "Not allowed Method", http.StatusMethodNotAllowed)
 		return
 	}
-	query := fmt.Sprintf("SELECT * FROM %s", tableName)
-	rows, err := db.Query(query)
-	if err != nil {
-		http.Error(w, "Server Error", http.StatusInternalServerError)
-		return
-	}
-	columns, err := rows.Columns()
-	if err != nil {
-		http.Error(w, "Error getting columns", http.StatusInternalServerError)
-	}
-	var results []T
-	t := reflect.TypeOf((*T)(nil)).Elem()
-	fieldMap := make(map[string]int)
-	for i := 0; i < t.NumField(); i++ {
-		field := t.Field(i)
-		jsonTag := field.Tag.Get("json")
-		if jsonTag != "" && jsonTag != "-" {
-			fieldMap[jsonTag] = i
-		}
-	}
-	fmt.Println(fieldMap)
-	for rows.Next() {
-		scanArgs := make([]interface{}, len(columns))
-		var item T
-		v := reflect.ValueOf(&item).Elem()
-		for i, col := range columns {
-			if fieldIndex, exists := fieldMap[col]; exists {
-				scanArgs[i] = v.Field(fieldIndex).Addr().Interface()
-			} else {
-				var dummy interface{}
-				scanArgs[i] = &dummy
-			}
-		}
-		if err := rows.Scan(scanArgs...); err != nil {
-			http.Error(w, "Error scanning row", http.StatusInternalServerError)
+	var item T
+	var rows *sql.Rows
+	var err error = nil
+	var result []any
+	switch any(item).(type) {
+	case Company:
+		company := any(item).(Company)
+		rows, err = db.Query(`SELECT * FROM companies`)
+		if err != nil {
+			http.Error(w, "Server Error", http.StatusInternalServerError)
+			log.Println(err)
 			return
 		}
-
-		results = append(results, item)
+		for rows.Next() {
+			err = rows.Scan(&company.ID, &company.Name, &company.CreatedAt, &company.UpdatedAt)
+			if err != nil {
+				http.Error(w, "Server Error", http.StatusInternalServerError)
+				continue
+			}
+			result = append(result, company)
+		}
+	case Candidate:
+		candidate := any(item).(Candidate)
+		rows, err = db.Query(`SELECT * FROM candidate`)
+		if err != nil {
+			http.Error(w, "Server Error", http.StatusInternalServerError)
+			log.Println(err)
+			return
+		}
+		for rows.Next() {
+			err = rows.Scan(&candidate.ID, &candidate.Name, &candidate.TelNumber, &candidate.Email, &candidate.CreatedAt, &candidate.UpdatedAt)
+			if err != nil {
+				http.Error(w, "Server Error", http.StatusInternalServerError)
+				continue
+			}
+			result = append(result, candidate)
+		}
+	case Posting:
+		idStr := r.PathValue("id")
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			http.Error(w, "Invalid id", http.StatusBadRequest)
+			return
+		}
+		post := any(item).(Posting)
+		rows, err = db.Query(`SELECT * FROM job_postings WHERE company_id = $1`, id)
+		if err != nil {
+			http.Error(w, "Server Error", http.StatusInternalServerError)
+			log.Println(err)
+			return
+		}
+		for rows.Next() {
+			err = rows.Scan(&post.ID, &post.CompanyID, &post.Title, &post.Description, &post.CreatedAt, &post.UpdatedAt)
+			if err != nil {
+				http.Error(w, "Server Error", http.StatusInternalServerError)
+				continue
+			}
+			result = append(result, post)
+		}
 	}
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(results)
+	json.NewEncoder(w).Encode(result)
 }
-func GetByID[T any](db *sql.DB, w http.ResponseWriter, r *http.Request, tableName string) {
+func GetByID[T any](db *sql.DB, w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		http.Error(w, "Not allowed Method", http.StatusMethodNotAllowed)
 		return
@@ -146,37 +127,59 @@ func GetByID[T any](db *sql.DB, w http.ResponseWriter, r *http.Request, tableNam
 		http.Error(w, "Invalid id", http.StatusBadRequest)
 		return
 	}
-	query := fmt.Sprintf("SELECT * from %s WHERE id = %d", tableName, id)
-	row := db.QueryRow(query)
 	var item T
-	t := reflect.TypeOf((*T)(nil)).Elem()
-	n := t.NumField()
-	fieldsMap := make(map[string]int)
-	for i := 0; i < n; i++ {
-		jsonTag := t.Field(i).Tag.Get("json")
-		if jsonTag != "" || jsonTag != "-" {
-			fieldsMap[jsonTag] = i
+	switch any(item).(type) {
+	case Company:
+		company := any(item).(Company)
+		row := db.QueryRow("SELECT * FROM companies WHERE id = $1", id)
+		err = row.Scan(&company.ID, &company.Name, &company.CreatedAt, &company.UpdatedAt)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				http.Error(w, "Not Fount", http.StatusNotFound)
+				return
+			}
+			http.Error(w, "Server Error", http.StatusInternalServerError)
+			return
 		}
-	}
-	scanArgs := make([]interface{}, n)
-	v := reflect.ValueOf(&item).Elem()
-	for i := 0; i < n; i++ {
-		jsonTag := t.Field(i).Tag.Get("json")
-		if filedIndex, exist := fieldsMap[jsonTag]; exist {
-			scanArgs[i] = v.Field(filedIndex).Addr().Interface()
-		} else {
-			var dummy interface{}
-			scanArgs[i] = &dummy
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(company)
+	case Candidate:
+		candidate := any(item).(Candidate)
+		row := db.QueryRow("SELECT * FROM candidate WHERE id = $1", id)
+		err = row.Scan(&candidate.ID, &candidate.Name, &candidate.TelNumber, &candidate.Email, &candidate.CreatedAt, &candidate.UpdatedAt)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				http.Error(w, "Not Fount", http.StatusNotFound)
+				return
+			}
+			http.Error(w, "Server Error", http.StatusInternalServerError)
+			return
 		}
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(candidate)
+	case Posting:
+		idStr = r.PathValue("job_id")
+		job, err := strconv.Atoi(idStr)
+		if err != nil {
+			http.Error(w, "Invalid id", http.StatusBadRequest)
+			return
+		}
+		post := any(item).(Posting)
+		row := db.QueryRow("SELECT * FROM job_postings WHERE id = $1", job)
+		err = row.Scan(&post.ID, &post.CompanyID, &post.Title, &post.Description, &post.CreatedAt, &post.UpdatedAt)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				http.Error(w, "Not Fount", http.StatusNotFound)
+				return
+			}
+			http.Error(w, "Server Error", http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(post)
 	}
-	if err := row.Scan(scanArgs...); err != nil {
-		http.Error(w, "Invalid id: "+err.Error(), http.StatusBadRequest)
-		return
-	}
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(item)
 }
-func Delete[T any](db *sql.DB, w http.ResponseWriter, r *http.Request, tableName string) {
+func Delete[T any](db *sql.DB, w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodDelete {
 		http.Error(w, "Not allowed Method", http.StatusMethodNotAllowed)
 		return
@@ -187,11 +190,33 @@ func Delete[T any](db *sql.DB, w http.ResponseWriter, r *http.Request, tableName
 		http.Error(w, "Invalid id", http.StatusBadRequest)
 		return
 	}
-	query := fmt.Sprintf("DELETE FROM %s WHERE id = %d", tableName, id)
-	result, err := db.Exec(query)
-	if err != nil {
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		return
+	var item T
+	var result sql.Result
+	switch any(item).(type) {
+	case Company:
+		result, err = db.Exec(`DELETE FROM companies WHERE id = $1`, id)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	case Candidate:
+		result, err = db.Exec(`DELETE FROM candidate WHERE id = $1`, id)
+		if err != nil {
+			http.Error(w, "Server Error", http.StatusInternalServerError)
+			return
+		}
+	case Posting:
+		idStr = r.PathValue("job_id")
+		job, err := strconv.Atoi(idStr)
+		if err != nil {
+			http.Error(w, "Invalid id", http.StatusBadRequest)
+			return
+		}
+		result, err = db.Exec(`DELETE FROM job_postings WHERE id = $1`, job)
+		if err != nil {
+			http.Error(w, "Server Error", http.StatusInternalServerError)
+			return
+		}
 	}
 	rowAffected, _ := result.RowsAffected()
 	if rowAffected == 0 {
@@ -200,7 +225,7 @@ func Delete[T any](db *sql.DB, w http.ResponseWriter, r *http.Request, tableName
 	}
 	w.WriteHeader(http.StatusOK)
 }
-func Update[T any](db *sql.DB, w http.ResponseWriter, r *http.Request, tableName string) {
+func Update[T any](db *sql.DB, w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPut {
 		http.Error(w, "Not allowed Method", http.StatusMethodNotAllowed)
 		return
@@ -217,20 +242,28 @@ func Update[T any](db *sql.DB, w http.ResponseWriter, r *http.Request, tableName
 		return
 	}
 	defer r.Body.Close()
-	t := reflect.TypeOf(item)
-	v := reflect.ValueOf(item)
-	n := t.NumField()
-	fieldsName := []string{}
-	var values []interface{}
-	counter := 0
-	for i := 0; i < n; i++ {
-		jsonTag := t.Field(i).Tag.Get("json")
-		if jsonTag == "-" {
-			continue
+	switch any(item).(type) {
+	case Company:
+		company := any(item).(Company)
+		_, err = db.Exec(`UPDATE companies SET name = $1, updated_at = NOW() WHERE id = $2`, company.Name, id)
+		if err != nil {
+			log.Println(err)
+			return
 		}
-		fieldsName = append(fieldsName, jsonTag)
-		values = append(values, v.Field(i).Interface())
+	case Candidate:
+		candidate := any(item).(Candidate)
+		_, err = db.Exec(`UPDATE candidate SET name = $1, tel_number = $2, email = $3, updated_at = NOW() WHERE id = $4`, candidate.Name, candidate.TelNumber, candidate.Email, id)
+		if err != nil {
+			http.Error(w, "Server Error", http.StatusInternalServerError)
+			return
+		}
+	case Posting:
+		post := any(item).(Posting)
+		_, err = db.Exec(`UPDATE job_postings SET title = $1, description = $2, updated_at = NOW() WHERE company_id = $3`, post.Title, post.Description, id)
+		if err != nil {
+			http.Error(w, "Server Error", http.StatusInternalServerError)
+			return
+		}
 	}
-	query := fmt.Sprintf("UPDATE %s set %s=%s WHERE id = %d")
-	result, err := db.Exec()
+	w.WriteHeader(http.StatusOK)
 }
