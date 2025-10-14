2585528efecd24fe44f684bd8d1fb1dcefb32d61
Шарифзода Икбол <iqbolsharifzoda724@gmail.com>
2025-10-14 15:01:00 +0300

    added error checking for func Affected

--- pkg/store/params.go
+++ pkg/store/params.go
@@ -2,7 +2,7 @@ package store
 
 import (
 	"database/sql"
-	"log"
+	"fmt"
 	"net/http"
 	"strconv"
 )
@@ -13,9 +13,6 @@ func MethodAllowed(w http.ResponseWriter, r *http.Request, MethodName string) {
 		return
 	}
 }
-func MethodStatus(w http.ResponseWriter, infoErr string, code int, err error) {
-	http.Error(w, infoErr, code)
-}
 func GetIDPath(w http.ResponseWriter, r *http.Request, nameID string) (int, bool) {
 	idStr := r.PathValue(nameID)
 	if idStr == "" {
@@ -24,14 +21,19 @@ func GetIDPath(w http.ResponseWriter, r *http.Request, nameID string) (int, bool
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
 		http.Error(w, "Invalid id", http.StatusBadRequest)
-		log.Fatal()
+		return 0, false
 	}
 	return id, true
 }
-func Affected(w http.ResponseWriter, result sql.Result) {
-	affected, _ := result.RowsAffected()
+func Affected(w http.ResponseWriter, result sql.Result) error {
+	affected, err := result.RowsAffected()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return err
+	}
 	if affected == 0 {
 		http.Error(w, "Not found", http.StatusNotFound)
-		return
+		return fmt.Errorf("no rows affected")
 	}
+	return nil
 }
